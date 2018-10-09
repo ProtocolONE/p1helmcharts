3c4fb767e1faae168a5128edb4aea23ee388095a
Valentin Vesvalo <vesvalo@mail.ru>
2018-10-09 14:58:02 +0300

    reindex

---
+++ reindex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -e
+
+pushd $(pwd)
+
+cd ./p1admin
+helm lint . && helm package .
+cd ../p1api
+helm lint . && helm package .
+cd ../p1auth
+helm lint . && helm package .
+
+cd ..
+helm repo index .
+
+popd
